fb6fa780c56e7b196ee3892fe2efc43b8ded2687
zahid gul <zahidgul107@gmail.com>
2023-01-10 21:47:08 +0530

    CRUD

---
+++ main.js
@@ -0,0 +1,73 @@
+// let form = document.getElementById('form');
+// let input = document.getElementById('input');
+// let msg = document.getElementById('msg');
+// let posts = document.getElementById('posts')
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log('button clicked');
+//     formValidation();
+// });
+
+// let formValidation = () => {
+//     if (input.value === "") {
+//         msg.innerHTML = "Post can't be blank";
+//         console.log("failure");
+//     } else {
+//         console.log("success");
+//         msg.innerHTML = "";
+//         acceptdata();
+//     }
+// }
+
+// let data = {}
+
+// let acceptdata = () => {
+//     data["text"] = input.value;
+//     console.log(data);
+//     createPost();
+
+// }
+
+// let createPost = () => {
+//     posts.innerHTML += `
+//     <div>
+//         <p>${data.text}</p>
+//         <span class="options">
+//             <i onClick="editPost(this)" class="fas fa-edit"></i>
+//             <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+//         </span>
+//     </div>
+//     `;
+//     input.value = "";
+// };
+
+// let deletePost = (e) => {
+//     e.parentElement.parentElement.remove();
+// }
+
+// let editPost = (e) => {
+//     input.value = e.parentElement.previousElementSibling.innerHTML;
+//     e.parentElement.parentElement.remove();
+// }
+
+// let form = document.getElementById('form');
+// let input = document.getElementById('input');
+// let msg = document.getElementById('msg');
+
+// form.addEventListener("submit", (e) =>{
+//     e.preventDefault();
+//     console.log("button clicked");
+//     formValidation();
+// });
+
+// let formValidation = () => {
+//     if (input.value === "") {
+//         msg.innerHTML = "Post can't be blank";
+//         console.log("failure");
+//     } else {
+//        msg.innerHTML = "";
+//        console.log("success"); 
+//     }
+// };
+
